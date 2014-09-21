863153e1d9730438a8e759482e5a8a41aa3bd845
Camille TEROL <camilleterol@gmail.com>
2014-09-21 21:26:37 +0200

    Rewrites the base code

--- reprise/main.c
+++ reprise/main.c
@@ -1,39 +1,38 @@
 /*  test de reprise de programmation apres un bon petit moment de non prog.
- on va partir sur un jeu de plus ou moins.
- */
+ on va partir sur un jeu de plus ou moins. */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+void emptyBuffer() {
+    char c = getchar();
+    while (c != '\n' && c != EOF) {
+        c = getchar();
+    }
+}
+
+char getOneChar() {
+    char c;
+    c = getchar();
+    emptyBuffer();
+    return c;
+}
+
+void selectMode() {
+    char nbPlayers;
+    nbPlayers = getOneChar();
+    switch (nbPlayers) {
+        case '1':
+            break;
+        case '2':
+            break;
+        default:
+            break;
+    }
+}
 
 int main(int argc, const char * argv[])
 {
-    int nombreDeJoueurs = 0;
-    
-    
-    while (nombreDeJoueurs != 1 && nombreDeJoueurs != 2)
-    {
-        printf("Combien de joueurs ont participer?\n");
-        printf("1. 1 joueur\n");
-        printf("2. 2 joueurs\n");
-    
-        scanf("%d", &nombreDeJoueurs);
-    
-        printf("\n");
-    
-        switch (nombreDeJoueurs) {
-            case 1:
-                printf("Mode 1 joueur en cours de programmation.\n");
-                nombreDeJoueurs = 1;
-                break;
-            case 2:
-                printf("Mode 2 joueurs en cours de programmation.\n");
-                nombreDeJoueurs = 2;
-                break;
-            default:
-                printf("Reponse incorrecte!\n\n\n");
-                break;
-        }
-    }
+    selectMode();
 }
